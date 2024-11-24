8df7e4f3976c7f7ea63c4d762d0b674861ab3d4d
Orsii1 <o.konyik@gmail.com>
2024-11-24 10:58:15 +0100

    Hibák javítása, kommentelve

--- main.cpp
+++ main.cpp
@@ -4,24 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //Hibás NELEMENTS 
-    std::cout << '1-100 ertekek duplazasa' // Hibás szintaxis
-    for (int i = 0;) // Hibás for ciklus
+    int *b = new int[N_ELEMENTS]; // Helyes változónév
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // Helyes szintaxis
+    for (int i = 0; i < N_ELEMENTS; i++) // Hibás feltétel javítása
     {
-        b[i] = i * 2; // Hibás ciklus feltétel
+        b[i] = (i + 1) * 2; // 1-től 100-ig duplázás
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++) // Hibás iteráció javítása
     {
-        std::cout << "Ertek:" // Hibás kimenet
-    }    
+        std::cout << "Ertek: " << b[i] << std::endl; // Pontos kimenet
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) // Hibás vessző 
+    int atlag = 0; // Inicializálás
+    for (int i = 0; i < N_ELEMENTS; i++) // Hibás vessző javítása
     {
-        atlag += b[i]
+        atlag += b[i]; // Összegzés
     }
-    atlag /= N_ELEMENTS;
+    atlag /= N_ELEMENTS; // Átlag számítása
     std::cout << "Atlag: " << atlag << std::endl;
-    // Memóriaszivárgás elkerülésének kihagyása
+
+    delete[] b; // Memóriaszivárgás elkerülése
     return 0;
 }
