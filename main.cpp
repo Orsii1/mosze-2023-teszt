3ca98e29f08a83f1f61288cc191cf9cf3780cf62
Orsii1 <o.konyik@gmail.com>
2024-11-24 10:31:40 +0100

    Hibák kikommentelése a kódban

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //Hibás NELEMENTS 
+    std::cout << '1-100 ertekek duplazasa' // Hibás szintaxis
+    for (int i = 0;) // Hibás for ciklus
     {
-        b[i] = i * 2;
+        b[i] = i * 2; // Hibás ciklus feltétel
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // Hibás kimenet
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // Hibás vessző 
     {
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    // Memóriaszivárgás elkerülésének kihagyása
     return 0;
 }
